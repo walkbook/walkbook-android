3c430b175b44d01d1e43fbb280af639fddb8c974
young924 <ahenaka9@gmail.com>
2022-01-05 18:03:01 +0900

    Get dangerous permissions on MainActivity

--- app/src/main/java/com/example/walkbookandroid/MainActivity.java
+++ app/src/main/java/com/example/walkbookandroid/MainActivity.java
@@ -18,9 +18,13 @@ import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.yanzhenjie.permission.Action;
+import com.yanzhenjie.permission.AndPermission;
+import com.yanzhenjie.permission.runtime.Permission;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +36,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getHashKey();
+
+        AndPermission.with(this)
+                .runtime()
+                .permission(Permission.ACCESS_FINE_LOCATION, Permission.ACCESS_COARSE_LOCATION)
+                .onGranted(new Action<List<String>>() {
+                    @Override
+                    public void onAction(List<String> data) {
+                        showToast("허용된 권한 개수: " + data.size());
+                    }
+                })
+                .onDenied(new Action<List<String>>() {
+                    @Override
+                    public void onAction(List<String> data) {
+                        showToast("거부된 권한 개수: " + data.size());
+                    }
+                })
+                .start();
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -45,21 +66,70 @@ public class MainActivity extends AppCompatActivity {
                 switch (menuItem.getItemId()) {
                     case R.id.mapItem:
                         getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new MapFragment()).commit();
-                        break;
+                        return true;
                     case R.id.postItem:
                         getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new PostFragment()).commit();
-                        break;
+                        return true;
                     case R.id.createItem:
                         getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new CreateFragment()).commit();
-                        break;
+                        return true;
                     case R.id.profileItem:
                         getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new ProfileFragment()).commit();
-                        break;
+                        return true;
                 }
-                return true;
+                return false;
             }
         });
+
+//        String[] permissions = {
+//                Manifest.permission.ACCESS_FINE_LOCATION,
+//                Manifest.permission.ACCESS_COARSE_LOCATION
+//        };
+//
+//        checkPermissions(permissions);
     }
+
+//    public void checkPermissions(String[] permissions) {
+//        ArrayList<String> targetList = new ArrayList<String>();
+//
+//        for (int i = 0; i < permissions.length; i++) {
+//            String curPermission = permissions[i];
+//            int permissionCheck = ContextCompat.checkSelfPermission(this, curPermission);
+//            if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+//                showToast(curPermission + " 권한 있음.");
+//            } else {
+//                showToast(curPermission + " 권한 없음.");
+//                if (ActivityCompat.shouldShowRequestPermissionRationale(this, curPermission)) {
+//                    showToast(curPermission + " 권한 설명 필요함.");
+//                } else {
+//                    targetList.add(curPermission);
+//                }
+//            }
+//        }
+//
+//        String[] targets = new String[targetList.size()];
+//        targetList.toArray(targets);
+//
+//        if (targets.length > 0) {
+//            ActivityCompat.requestPermissions(this, targets, 101);
+//        }
+//    }
+//
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+//        switch (requestCode) {
+//            case 101: {
+//                if (grantResults.length > 0 &&
+//                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    showToast("첫 번째 권한을 사용자가 승인함.");
+//                } else {
+//                    showToast("첫 번째 권한 거부됨.");
+//                }
+//
+//                return;
+//            }
+//        }
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
