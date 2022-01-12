7d006c5767eb73656f53d9e0b756595f77fc2245
young924 <ahenaka9@gmail.com>
2022-01-12 17:26:10 +0900

    Change dependencies and fix errors

--- app/src/main/java/com/example/walkbookandroid/LoginFragment.java
+++ app/src/main/java/com/example/walkbookandroid/LoginFragment.java
@@ -11,22 +11,6 @@ import android.widget.EditText;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-import com.google.gson.Gson;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.UnsupportedEncodingException;
-import java.util.HashMap;
-import java.util.Map;
-
 public class LoginFragment extends Fragment {
     LoginActivity activity;
     EditText editId;
@@ -62,13 +46,14 @@ public class LoginFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 LoginActivity activity = (LoginActivity) getActivity();
+                if (activity == null) {
+                    Log.e("Login", "failed to load LoginActivity");
+                    return;
+                }
+
                 activity.onFragmentChanged(0);
             }
         });
-
-        if (requestQueue == null) {
-            requestQueue = Volley.newRequestQueue(activity);
-        }
 
         return rootView;
     }
