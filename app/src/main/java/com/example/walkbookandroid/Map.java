cbf547ef707ca7f347d83b3ad032a15f2c04aec0
young924 <ahenaka9@gmail.com>
2022-01-25 18:37:21 +0900

    Create Map class and refactor map fragment

---
+++ app/src/main/java/com/example/walkbookandroid/Map.java
@@ -0,0 +1,77 @@
+package com.example.walkbookandroid;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapView;
+
+public class Map {
+    MainActivity activity;
+    MapView mapView;
+    MapPOIItem currentMarker;
+
+    public Map(MainActivity activity) {
+        this.activity = activity;
+        this.mapView = new MapView(activity);
+    }
+
+    public MapView getMapView() {
+        return mapView;
+    }
+
+    public void startLocationService() {
+        LocationManager manager = (LocationManager) activity.getSystemService(Context.LOCATION_SERVICE);
+
+        try {
+            Location location = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (location != null) {
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+
+                mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(latitude, longitude), true);
+
+                createCurrentMarker(latitude, longitude);
+            }
+
+            GPSListener gpsListener = new GPSListener();
+            long minTime = 10000;
+            float minDistance = 0;
+
+            manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime, minDistance, gpsListener);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createCurrentMarker(double latitude, double longitude) {
+        currentMarker = new MapPOIItem();
+        currentMarker.setItemName("현재 위치");
+        currentMarker.setTag(0);
+        currentMarker.setMapPoint(MapPoint.mapPointWithGeoCoord(latitude, longitude));
+        currentMarker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
+        currentMarker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+
+        mapView.addPOIItem(currentMarker);
+    }
+
+    class GPSListener implements LocationListener {
+        public void onLocationChanged(Location location) {
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+
+            mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(latitude, longitude), true);
+            activity.showToast("latitude : " + latitude + ", longitude : " + longitude);
+        }
+
+        public void onProviderDisabled(String provider) { }
+
+        public void onProviderEnabled(String provider) { }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) { }
+    }
+}
